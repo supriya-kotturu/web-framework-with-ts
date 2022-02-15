985546ad2ad10f01202f696421efee37139f1fa2
supriya-kotturu <supriyakotturu@pm.me>
2022-02-15 22:11:58 +0530

    refactor : extract Event related code from User

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,10 +6,7 @@ interface UserProps {
 	age: number;
 }
 
-type Callback = () => void;
-
 export class User {
-	private _events: { [key: string]: Callback[] } = {};
 	constructor(private data: UserProps) {}
 
 	get<K extends keyof UserProps>(propName: K): string | number | undefined {
@@ -18,18 +15,6 @@ export class User {
 
 	set(update: Partial<UserProps>): void {
 		Object.assign(this.data, update);
-	}
-
-	on(eventName: string, callback: Callback): void {
-		const handlers = this._events[eventName] || [];
-		handlers.push(callback);
-		this._events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this._events[eventName];
-		if (!handlers || handlers.length === 0) return;
-		handlers.forEach((callback) => callback());
 	}
 
 	fetch(): void {
